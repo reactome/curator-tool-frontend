6bb28ee4b609621ba79fa608fc0dacb3d19cfa94
guanmingwu <guanmingwu@gmail.com>
2023-11-03 21:09:44 -0700

    Use rxjs to make two dataservice calls

--- src/app/list-instances/components/table/list-instances-table.component.ts
+++ src/app/list-instances/components/table/list-instances-table.component.ts
@@ -4,6 +4,7 @@ import { MatTableDataSource } from "@angular/material/table";
 import { ActivatedRoute } from "@angular/router";
 import { Instance } from "../../../core/models/reactome-instance.model";
 import { DataService } from "../../../core/services/data.service";
+import { concatMap } from 'rxjs';
 
 @Component({
   selector: 'app-list-instances-table',
@@ -15,7 +16,7 @@ export class ListInstancesTableComponent implements OnInit {
   matDataSource = new MatTableDataSource<Instance>();
   skip: number = 0;
   // For doing search
-  searchKey: string|undefined = undefined;
+  searchKey: string | undefined = undefined;
   pageSizeOptions = [20, 50, 100];
   pageSize: number = 20;
   className: any = "";
@@ -33,36 +34,35 @@ export class ListInstancesTableComponent implements OnInit {
   ngOnInit(): void {
     // console.log('inputClassName' + this.inputClassName)
     this.isSelection ? this.className = this.inputClassName && this.loadInstances() :
-    this.route.params.subscribe((className) => {
-      this.className = className;
-      this.className = this.className.className;
-      this.skip = 0;
-      this.pageIndex = 0;
-      this.loadInstances();
-    });
+      this.route.params.subscribe((className) => {
+        this.className = className;
+        this.className = this.className.className;
+        this.skip = 0;
+        this.pageIndex = 0;
+        this.loadInstances();
+      });
   }
 
-  //TODO: There is one error related to reset paginator in debug:
-//   arg1:
-// Error: NG0100: ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked. Previous value: '0 of 0'. Current value: '1 – 20 of 20'. Find more at https://angular.io/errors/NG0100\n    at throwErrorIfNoChangesMode (http://localhost:4200/vendor.js:60050:9)\n    at bindingUpdated (http://localhost:4200/vendor.js:64222:9)\n    at interpolation1 (http://localhost:4200/vendor.js:64315:21)\n    at Module.ɵɵtextInterpolate1 (http://localhost:4200/vendor.js:67460:24)\n    at MatPaginator_Template (http://localhost:4200/vendor.js:102309:71)\n    at executeTemplate (http://localhost:4200/vendor.js:61929:5)\n    at refreshView (http://localhost:4200/vendor.js:61813:7)\n    at refreshComponent (http://localhost:4200/vendor.js:62827:7)\n    at refreshChildComponents (http://localhost:4200/vendor.js:61611:5)\n    at refreshView (http://localhost:4200/vendor.js:61863:7) {code: -100, stack: 'Error: NG0100: ExpressionChangedAfterItHasB… (http://localhost:4200/vendor.js:61863:7)', …}
-// handleError @ /Users/wug/git/reactome/curator-tool-frontend/node_modules/@angular/core/fesm2020/core.mjs:8400:23
   loadInstances() {
-    // Make sure getInstanceCount first and then listInstance so that there is no reset of paginator.length.
-    this.dataService.getInstanceCount(this.className, this.searchKey).subscribe(count => {
-      this.instanceCount = count, 
-      this.dataService.listInstances(this.className, this.skip, this.pageSize, this.searchKey).subscribe(instanceList => {
-        this.matDataSource.data = instanceList;
-        this.matDataSource.paginator = this.paginator;
-        // Based on this: https://www.freakyjolly.com/angular-material-12-server-side-table-pagination-example/
-        // Use timeout to reset the paginator.
-        setTimeout(() => {
-          this.paginator.pageIndex = this.pageIndex;
-          this.paginator.pageSize = this.pageSize;
-          this.paginator.length = this.instanceCount;
+    this.dataService.getInstanceCount(this.className, this.searchKey)
+      .pipe(
+        concatMap(count => {
+          this.instanceCount = count;
+          return this.dataService.listInstances(this.className, this.skip, this.pageSize, this.searchKey);
+        })
+      )
+      .subscribe(
+        (instanceList) => {
+          this.matDataSource.data = instanceList;
+          this.matDataSource.paginator = this.paginator;
+          // Based on this: https://www.freakyjolly.com/angular-material-12-server-side-table-pagination-example/
+          // Use timeout to reset the paginator.
+          setTimeout(() => {
+            this.paginator.pageIndex = this.pageIndex;
+            this.paginator.pageSize = this.pageSize;
+            this.paginator.length = this.instanceCount;
+          });
         });
-      } 
-      );
-    });
   }
 
   searchForName(event: Event) {
