3986a5c45e35c5881b8b96e0b51875555103a496
guanmingwu <guanmingwu@gmail.com>
2023-11-03 18:40:37 -0700

    avoid reset the total counts

--- src/app/list-instances/components/table/list-instances-table.component.ts
+++ src/app/list-instances/components/table/list-instances-table.component.ts
@@ -20,6 +20,7 @@ export class ListInstancesTableComponent implements OnInit, AfterViewInit {
   pageSize: number = 20;
   className: any = "";
   pageIndex: number = 0;
+  instanceCount: number = 0;
   @Input() inputClassName: string = "";
   @Input() isSelection: boolean = false;
 
@@ -50,17 +51,22 @@ export class ListInstancesTableComponent implements OnInit, AfterViewInit {
 //   arg1:
 // Error: NG0100: ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked. Previous value: '0 of 0'. Current value: '1 – 20 of 20'. Find more at https://angular.io/errors/NG0100\n    at throwErrorIfNoChangesMode (http://localhost:4200/vendor.js:60050:9)\n    at bindingUpdated (http://localhost:4200/vendor.js:64222:9)\n    at interpolation1 (http://localhost:4200/vendor.js:64315:21)\n    at Module.ɵɵtextInterpolate1 (http://localhost:4200/vendor.js:67460:24)\n    at MatPaginator_Template (http://localhost:4200/vendor.js:102309:71)\n    at executeTemplate (http://localhost:4200/vendor.js:61929:5)\n    at refreshView (http://localhost:4200/vendor.js:61813:7)\n    at refreshComponent (http://localhost:4200/vendor.js:62827:7)\n    at refreshChildComponents (http://localhost:4200/vendor.js:61611:5)\n    at refreshView (http://localhost:4200/vendor.js:61863:7) {code: -100, stack: 'Error: NG0100: ExpressionChangedAfterItHasB… (http://localhost:4200/vendor.js:61863:7)', …}
 // handleError @ /Users/wug/git/reactome/curator-tool-frontend/node_modules/@angular/core/fesm2020/core.mjs:8400:23
-  loadInstances(){
-    this.dataService.listInstances(this.className, this.skip, this.pageSize, this.searchKey).subscribe(instanceList => {
-      this.matDataSource.data = instanceList;
-      // Based on this: https://www.freakyjolly.com/angular-material-12-server-side-table-pagination-example/
-      // Use timeout to reset the paginator.
-      setTimeout(() => {
-        this.paginator.pageIndex = this.pageIndex;
-        this.paginator.pageSize = this.pageSize;
-        this.dataService.getInstanceCount(this.className, this.searchKey).subscribe(count => this.paginator.length = count)
-      });
-    })
+  loadInstances() {
+    // Make sure getInstanceCount first and then listInstance so that there is no reset of paginator.length.
+    this.dataService.getInstanceCount(this.className, this.searchKey).subscribe(count => {
+      this.instanceCount = count, 
+      this.dataService.listInstances(this.className, this.skip, this.pageSize, this.searchKey).subscribe(instanceList => {
+        this.matDataSource.data = instanceList;
+        // Based on this: https://www.freakyjolly.com/angular-material-12-server-side-table-pagination-example/
+        // Use timeout to reset the paginator.
+        setTimeout(() => {
+          this.paginator.pageIndex = this.pageIndex;
+          this.paginator.pageSize = this.pageSize;
+          this.paginator.length = this.instanceCount;
+        });
+      } 
+      );
+    });
   }
 
   searchForName(event: Event) {
