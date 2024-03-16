1f6ca91f4e95d5f0ba5ee080ab82225ff5e8b962
guanmingwu <guanmingwu@gmail.com>
2024-03-16 13:06:00 -0700

    set up the framework for using LLM-base curation

--- src/app/main/components/text-curation/text-curation.component.ts
+++ src/app/main/components/text-curation/text-curation.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { MatLabel } from '@angular/material/form-field';
 import { ChatOpenAI } from '@langchain/openai';
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { DataService } from 'src/app/core/services/data.service';
+import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { NewInstanceActions } from 'src/app/instance/state/new-instance/new-instance.actions';
 
 @Component({
   selector: 'app-text-curation',
@@ -13,24 +17,72 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 export class TextCurationComponent {
 
   // Test code: Need to remove it
-  chatModel = new ChatOpenAI(
+  chatModel: ChatOpenAI = new ChatOpenAI(
     {
       openAIApiKey: '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',
+      modelName: 'gpt-3.5-turbo',
+      temperature: 0,
     }
-  )
+  );
 
-  prompt = ChatPromptTemplate.fromMessages([
-    ["system", "You are a world class biologist."],
-    ["user", "{input}"],
-  ]);
+  operationSchema = {
+    type: "object",
+    properties: {
+      operation: {
+        type: "string",
+        enum: ["createNewInstance", "setAttribute", "searchInstance", "editInstance"],
+      },
+      schemaClassName: { type: "string" },
+      attribute: { type: "string" },
+      value: { type: "string" },
+    },
+    required: ["operation", "schemaClassName", "attribute", "value"],
+  };
+
+  constructor(private dataSerice: DataService,
+    private objectStore: Store,
+    private router: Router) { }
 
   async execute(command: string) {
     console.debug('Execute: ', command);
-    const chain = this.prompt.pipe(this.chatModel);
+    const modelWithStructuredOutput = this.chatModel.withStructuredOutput(this.operationSchema);
+    const prompt = ChatPromptTemplate.fromMessages([
+      ['system',
+        `Extract the operations and their related arguements from the user's input below.
+        Respond with a JSON object containing required keys:
+        'operation': the type of operation to execute.
+        if the operation is createNewInstance, provide 'schemaClassName', which is the class name of the new instance.
+        If the operation is setAttribute, provide two arguments: attribute for the property name and value for the property
+        value. Make sure the class names and attribute names follow the standard object-oriented class and property naming
+        convention using "CAMELCASE", e.g., class names should start with Upper case.
+        `
+      ],
+      ['user', '{command}']
+    ]
+    );
+    const chain = prompt.pipe(modelWithStructuredOutput);
     let answer = await chain.invoke({
-      input: 'Tell me something about gene TANC1',
-    });
+      command: command,
+    })
     console.debug('Answer: ', answer);
+    if (answer['operation'] === 'createNewInstance') {
+      if (answer['schemaClassName']) {
+        // Need to normalize the class name
+        this.createNewInstance(answer['schemaClassName']);
+      }
+    }
+  }
+
+  //TODO: Copied from schema-class-tree.component.ts. Need refactor to create a new service so that all functions
+  // used here can be shared across the whole application.
+  createNewInstance(schemaClassName: string) {
+    this.dataSerice.createNewInstance(schemaClassName).subscribe(instance => {
+      this.dataSerice.registerNewInstance(instance);
+      this.objectStore.dispatch(NewInstanceActions.register_new_instances(instance));
+      let dbId = instance.dbId.toString();
+      this.router.navigate(["/instance_view/" + dbId]);
+    });
   }
 
 }
+
