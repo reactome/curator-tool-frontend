7ca60bb3c2f01433d5fccbc7c1ae1c1dd983bbe1
guanmingwu <guanmingwu@gmail.com>
2024-04-01 11:03:47 -0700

    updates for llm-based function calls

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -28,6 +28,7 @@ export class DataService {
   private entityDataUrl = `${environment.ApiRoot}/findByDbId/`;
   private schemaClassTreeUrl = `${environment.ApiRoot}/getSchemaClassTree/`;
   private listInstancesUrl = `${environment.ApiRoot}/listInstances/`;
+  private findInstanceByDisplayNameUrl = `${environment.ApiRoot}/findByDisplayName`;
   private countInstancesUrl = `${environment.ApiRoot}/countInstances/`;
   private commitInstanceUrl = `${environment.ApiRoot}/commit/`;
   // Track the negative dbId to be used
@@ -46,36 +47,6 @@ export class DataService {
    * @returns
    */
   fetchSchemaClass(className: string): Observable<SchemaClass> {
-    // Check cached results first
-    if (this.name2SchemaClass.has(className)) {
-      return of(this.name2SchemaClass.get(className)!);
-    }
-    // Otherwise call the restful API
-    return this.http.get<SchemaClass>(this.schemaClassDataUrl + `${className}`)
-      .pipe(
-        map((data: any) => {
-          // console.log("fetchSchemaClass:");
-          // console.log(data);
-          // convert data to schemaClass
-          let schemaCls = this.convertToSchemaClass(className, data);
-          this.name2SchemaClass.set(schemaCls.name, schemaCls);
-          return schemaCls;
-        }),
-        catchError((err: Error) => {
-          console.log("The dataset options could not been loaded: \n" + err.message, "Close", {
-            panelClass: ['warning-snackbar'],
-            duration: 10000
-          });
-          return throwError(() => err);
-        }));
-  }
-
-  /**
-   * Fetch the instance data.
-   * @param className
-   * @returns
-   */
-  fetchEvent(className: string): Observable<SchemaClass> {
     // Check cached results first
     if (this.name2SchemaClass.has(className)) {
       return of(this.name2SchemaClass.get(className)!);
@@ -127,37 +98,10 @@ export class DataService {
         }));
   }
 
-  /**
-   * Fetch the schema class table.
-   * @param className
-   * @returns
-   */
-  fetchEventTree(skipCache: boolean): Observable<SchemaClass> {
-    //Check cached results first
-    if (this.rootClass && !skipCache) {
-      return of(this.rootClass!);
-    }
-    // Otherwise call the restful API
-    return this.http.get<SchemaClass>(this.schemaClassTreeUrl)
-      .pipe(
-        map((data: SchemaClass) => {
-          // console.debug("fetchSchemaClassTree:", data);
-          this.rootClass = data;
-          return this.rootClass;
-        }),
-        catchError((err: Error) => {
-          console.log("The schema class table could not been loaded: \n" + err.message, "Close", {
-            panelClass: ['warning-snackbar'],
-            duration: 10000
-          });
-          return throwError(() => err);
-        }));
-  }
-
   getSchemaClass(clsName: string): SchemaClass | undefined {
     if (this.name2class && this.name2class.size > 0) {
-    return this.name2class.get(clsName);
-  }
+      return this.name2class.get(clsName);
+    }
     this.name2class = new Map<string, SchemaClass>();
     if (this.rootClass)
       this.buildSchemaClassMap(this.rootClass, this.name2class);
@@ -411,6 +355,28 @@ export class DataService {
   }
 
   /**
+   * Find an Instance based on its display name and a list of class names. 
+   * @param displayName
+   * @param className 
+   */
+  findInstanceByDisplayName(displayName: string,
+    className: string[]): Observable<Instance> {
+    let clsNameText = className.join(',');
+    // The URL should encode itself
+    let url = this.findInstanceByDisplayNameUrl + '?displayName=' + displayName + "&classNames=" + clsNameText;
+    // console.debug('list instances url: ' + url);
+    return this.http.get<Instance>(url)
+      .pipe(map((data: Instance) => data), // Nothing needs to be done.
+        catchError((err: Error) => {
+          console.log("No instance can be found: \n" + err.message, "Close", {
+            panelClass: ['warning-snackbar'],
+            duration: 10000
+          });
+          return throwError(() => err);
+        }));
+  }
+
+  /**
    * Commit the passed instance back to the database.
    * @param instance
    */
@@ -429,7 +395,7 @@ export class DataService {
   }
 
   private cloneInstanceForCommit(source: Instance): Instance {
-    let instance : Instance = {
+    let instance: Instance = {
       dbId: source.dbId,
       displayName: source.displayName,
       schemaClassName: source.schemaClassName,
@@ -443,10 +409,10 @@ export class DataService {
   }
 
   // TODO: Create a separate service for instance/attribute logic
-  setCandidateClasses(schemaAttribute: SchemaAttribute): any[]  {
+  setCandidateClasses(schemaAttribute: SchemaAttribute): any[] {
     // @ts-ignore
     let concreteClassNames = new Set<string>();
-    if(schemaAttribute.allowedClases) {
+    if (schemaAttribute.allowedClases) {
       for (let clsName of schemaAttribute.allowedClases) {
         let schemaClass: SchemaClass = this.getSchemaClass(clsName)!;
         this.grepConcreteClasses(schemaClass, concreteClassNames);
--- src/app/schema-view/main-schema-view/components/text-curation/text-curation.component.ts
+++ src/app/schema-view/main-schema-view/components/text-curation/text-curation.component.ts
@@ -8,12 +8,14 @@ import { ChatOpenAI } from '@langchain/openai';
 import { Store } from '@ngrx/store';
 import { AgentExecutor, createOpenAIFunctionsAgent } from "langchain/agents";
 import { pull } from 'langchain/hub';
+import { AttributeDataType, SchemaAttribute } from 'src/app/core/models/reactome-schema.model';
 import { DataService } from 'src/app/core/services/data.service';
 import { InstanceTableComponent } from 'src/app/schema-view/instance/components/instance-view/instance-table/instance-table.component';
 import { NewInstanceActions } from 'src/app/schema-view/instance/state/new-instance/new-instance.actions';
 import { z } from "zod";
 
-import { DynamicStructuredTool, DynamicTool } from "@langchain/core/tools";
+import { DynamicStructuredTool } from "@langchain/core/tools";
+import { Instance } from 'src/app/core/models/reactome-instance.model';
 
 @Component({
   selector: 'app-text-curation',
@@ -31,7 +33,8 @@ export class TextCurationComponent {
   // Test code: Need to remove it
   chatModel: ChatOpenAI = new ChatOpenAI(
     {
-      openAIApiKey: '{}', // This is needed. Right now it is hard-coded. To be pulled from the server-side.
+      // !!!Don't push this key to GitHub!!!
+      openAIApiKey: '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', // This is needed. Right now it is hard-coded. To be pulled from the server-side.
       modelName: 'gpt-3.5-turbo',
       temperature: 0,
     }
@@ -47,16 +50,16 @@ export class TextCurationComponent {
       schemaClassName: { type: "string" },
       attribute: { type: "string" },
       value: { type: "string" },
-      dbId: { type: "number"}
+      dbId: { type: "number" }
     },
     required: ["operation", "schemaClassName", "attribute", "value", "dbId"],
   };
 
-  constructor(private dataSerice: DataService,
+  constructor(private dataService: DataService,
     private objectStore: Store,
     private router: Router) { }
 
-  // The following implementation is based on:
+  // The following implementation is based on: 
   // https://js.langchain.com/docs/modules/agents/tools/dynamic
   async execute(command: string) {
     console.debug('Execute: ', command);
@@ -69,14 +72,14 @@ export class TextCurationComponent {
         schema: z.object({
           schemaClassName: z.string().describe('The schema class name of the new instance'),
         }),
-        func: async ({schemaClassName}) => {
+        func: async ({ schemaClassName }) => {
           this.createNewInstance(schemaClassName);
           return 'a new instance is created.';
         },
       }),
       new DynamicStructuredTool({
-        name: "setAttribute",
-        description: "Set the attribute value for the displayed instance",
+        name: "setAttributeValue",
+        description: "Set the attribute value for the displayed instance based on the attribute name.",
         schema: z.object({
           attributeName: z.string().describe("The attribute name"),
           value: z.any().describe("The attribute value"),
@@ -88,24 +91,48 @@ export class TextCurationComponent {
         },
       }),
       new DynamicStructuredTool({
-        name: "editInstance",
-        description: "Open the instance specified by its dbId for editing",
+        name: "addAttributeValue",
+        description: "Add the attribute value for the displayed instance based on the attribute name.",
         schema: z.object({
-          dbId: z.number().describe("The dbId of the instance to be edited"),
+          attributeName: z.string().describe("The attribute name"),
+          value: z.any().describe("The attribute value"),
         }),
-        func: async ({dbId}) => {
+        func: async ({ attributeName, value }) => {
+          console.debug('Attriute name: ', attributeName, 'value: ', value);
+          this.setAttribute(attributeName, value, true);
+          return 'assign attribute done';
+        },
+      }),
+      new DynamicStructuredTool({
+        name: "editInstance",
+        description: "Open the instance specified by its dbId for editing or display name and class name",
+        schema: z.object({
+          dbId: z.number().describe("The dbId of the instance to be edited")
+          // className: z.string().optional().describe('The className of the instance to be edited'),
+          // displayName: z.string().optional().describe('The displayName of the instance to be edited') 
+        }),
+        func: async ({ dbId }) => {
           console.debug('Instance to be edited: ', dbId);
           this.editInstance(dbId);
           return "instance view opened";
         },
       }),
     ];
-    // Get the prompt to use - you can modify this!\
+    // Get the prompt to use - you can modify this!
     // If you want to see the prompt in full, you can at:
     // https://smith.langchain.com/hub/hwchase17/openai-functions-agent
-    const prompt = await pull<ChatPromptTemplate>(
+    //TODO: Have to find a way to customize this by inject something to system!
+    let prompt = await pull<ChatPromptTemplate>(
       "hwchase17/openai-functions-agent"
     );
+
+    //     const prompt = ChatPromptTemplate.fromTemplate("SYSTEM: Extract verbs first and map these verbs to functions \
+    // as following: set -> setAttribute, create -> createNewInstance, edit -> editInstance. Do not \
+    // call any functions that are not mentioned in the command. If you find a class name but no create,\
+    // don't invoke createNewInstance! For the schema class name found for createNewInstance, format it \
+    // into UpperCamelCase, with the first letter of every word capitalized. Don't do this format for other functions.\
+    // \n\nHUMAN: {input}\n\n{agent_scratchpad}");
+
     const llm = this.chatModel;
     const agent = await createOpenAIFunctionsAgent({
       llm,
@@ -119,8 +146,17 @@ export class TextCurationComponent {
       verbose: true,
     });
 
+    // Need to add something to limit the function.
+    // The following cannot work. Need a short and very succint instruction!
+    //     let input = "command: " + command + "\n\n" + "Important note: Use the verb in the command only to find function. Don't use any verb not in the command. \
+    // If you find a class name but no creation, don't invoke createNewInstance! For the schema \
+    // class name found for createNewInstance, format it \
+    // into UpperCamelCase, with the first letter of every word capitalized. Don't do this format for other functions. The output should be just the found function and parametmers. Nothing else!";
+    // let input = command;
+    // Cannot add too much text here. Otherwise, LLM will also output too much information the function cannot be called.
+    let input = "command: " + command + "\n\n" + "Important note: Don't call createNewInstance for set or add!";
     const result = await agentExecutor.invoke({
-      input: command,
+      input: input,
     });
 
     console.debug("Got output ", result['output']);
@@ -129,8 +165,8 @@ export class TextCurationComponent {
   //TODO: Copied from schema-class-tree.component.ts. Need refactor to create a new service so that all functions
   // used here can be shared across the whole application.
   createNewInstance(schemaClassName: string) {
-    this.dataSerice.createNewInstance(schemaClassName).subscribe(instance => {
-      this.dataSerice.registerNewInstance(instance);
+    this.dataService.createNewInstance(schemaClassName).subscribe(instance => {
+      this.dataService.registerNewInstance(instance);
       this.objectStore.dispatch(NewInstanceActions.register_new_instances(instance));
       let dbId = instance.dbId.toString();
       this.router.navigate(["/instance_view/" + dbId]);
@@ -138,19 +174,115 @@ export class TextCurationComponent {
   }
 
   editInstance(dbId: number) {
-    this.router.navigate(["/instance_view/" + dbId]);
+    if (dbId !== undefined)
+      this.router.navigate(["/instance_view/" + dbId]);
   }
 
-  setAttribute(attribute: string,
-               value: any) {
+  setAttribute(attribute: string, value: any, append: boolean = false) {
     if (this.instanceTable === undefined || this.instanceTable._instance === undefined)
       return;
     // Make sure the value is validated
     const instance = this.instanceTable._instance;
-    instance.attributes.set(attribute, [value]);
+    // Determinte if the attribute is an instance type
+    const clsAtt = this.getClassAttribute(attribute, instance);
+    if (clsAtt === undefined) return;
+    if (clsAtt.type !== AttributeDataType.INSTANCE) {
+      if (clsAtt.cardinality === '1')
+        instance.attributes.set(attribute, value);
+      else {
+        if (append) {
+          let values = instance.attributes.get(attribute);
+          if (values)
+            values.push(value);
+          else
+            instance.attributes.set(attribute, [value]);
+        }
+        else
+          instance.attributes.set(attribute, [value]);
+      }
+      this.validateTable(attribute);
+    }
+    else {
+      this.setInstanceAttribute(clsAtt, value, instance, append);
+    }
+  }
+
+  private validateTable(attributeName: string) {
+    if (this.instanceTable === undefined) return;
     this.instanceTable.updateTableContent();
     //TODO: This function should be refactor to something related to Instance only, not in the table!
-    this.instanceTable.addModifiedAttributeName(attribute);
+    this.instanceTable.addModifiedAttributeName(attributeName);
+  }
+
+  /**
+   * Find an instance that matches display name as passed or dbId. 
+   * @param clsAtt 
+   * @param value 
+   * @returns 
+   */
+  private setInstanceAttribute(clsAtt: SchemaAttribute,
+    value: string,
+    instance: Instance,
+    append: boolean = false) {
+    if (typeof value === 'number') {
+      // This should be a DB_ID of the instance
+      const dbId = value as number;
+      this.dataService.fetchInstance(dbId).subscribe(attInst => {
+        this._setInstanceAttribute(instance, clsAtt, attInst, append);
+      });
+    }
+    else { // display name is assumed
+      this.dataService.findInstanceByDisplayName(value, clsAtt.allowedClases!).subscribe(attInst => {
+        this._setInstanceAttribute(instance, clsAtt, attInst, append);
+      })
+    }
+  }
+
+  // A helper function to set the instance type of attribute actually.
+  private _setInstanceAttribute(instance: Instance,
+    clsAtt: SchemaAttribute,
+    attInst: Instance,
+    append: boolean = false) {
+    if (attInst === undefined)
+      return;
+    // We just need the following information for an instance value
+    const attValue = {
+      dbId: attInst.dbId,
+      displayName: attInst.displayName,
+      schemaClassName: attInst.schemaClassName
+    }
+    if (clsAtt.cardinality == '1')
+      instance.attributes.set(clsAtt.name, attValue);
+    else {
+      if (append) {
+        let values = instance.attributes.get(clsAtt.name);
+        if (values) 
+          values.push(attValue);
+        else
+          instance.attributes.set(clsAtt.name, [attValue]);
+      }
+      else
+        instance.attributes.set(clsAtt.name, [attValue]);
+    }
+    this.validateTable(clsAtt.name);
+  }
+
+  /**
+   * Perform a linear search to find the attribute defined in the instance's class.
+   * @param attName 
+   * @param instance 
+   * @returns 
+   */
+  private getClassAttribute(attName: string,
+    instance: Instance): SchemaAttribute | undefined {
+    if (instance.schemaClass?.attributes) {
+      for (let att of instance.schemaClass?.attributes) {
+        if (att.name === attName) {
+          return att;
+        }
+      }
+    }
+    return undefined;
   }
 
 }
