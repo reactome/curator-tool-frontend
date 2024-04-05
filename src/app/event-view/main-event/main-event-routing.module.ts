52702a67ef5463f6ca3dea971d0bffbbd4de9edb
Robert Petryszak <info@datasome.co.uk>
2024-04-05 14:47:56 +0100

    Routed instance view from Event tree to the Schema View for now - pending further work

--- src/app/event-view/main-event/main-event-routing.module.ts
+++ src/app/event-view/main-event/main-event-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {MainEventComponent} from "./main-event.component";
+import {MainSchemaViewComponent} from "../../schema-view/main-schema-view/main-schema-view.component";
 
 const routes: Routes = [
   {
-    path: ``,
+    path: `instance`,
+    component: MainSchemaViewComponent,
+    loadChildren: () =>
+      import("../../schema-view/instance/instance.module").then((m) =>
+        m.InstanceModule),
+  },
+  {
+    path: '',
     component: MainEventComponent
   }
 ]
