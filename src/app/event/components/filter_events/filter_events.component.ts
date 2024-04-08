1e2d86d560c8d69d0e8351bc3cb2551d58753d44
Robert Petryszak <info@datasome.co.uk>
2024-04-08 16:05:22 +0100

    In event tree filters, fixed to populate class names and attributes dynamically

--- src/app/event/components/filter_events/filter_events.component.ts
+++ src/app/event/components/filter_events/filter_events.component.ts
@@ -3,6 +3,8 @@ import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {DataService} from "../../../core/services/data.service";
+import {SchemaClass} from "../../../core/models/reactome-schema.model";
 
 interface Species {
   value: string;
@@ -22,9 +24,11 @@ interface Species {
 export class FilterEventsComponent {
   // For doing search
   searchKey: string | undefined = undefined;
-
-  @Output() updateEventTree = new EventEmitter<Array<string | undefined>>();
   selectedSpecies = "All";
+  selectedClass = "Event";
+  selectedAttribute = "displayName";
+  selectedOperand = "Equals";
+  // TODO: There are many more species in Neo4J than those below (offered as filters in CuratorTool)
   species: Species[] = [
     {value: 'All', viewValue: 'All'},
     {value: 'Homo sapiens', viewValue: 'Homo sapiens'},
@@ -35,41 +39,8 @@ export class FilterEventsComponent {
     {value: 'Fugu Rubripes', viewValue: 'Fugu Rubripes'},
     {value: 'Mus musculus', viewValue: 'Mus musculus'}
   ];
-
-  // TODO:  Retrieve all recursive children of Event dynamically from the Schema
-  selectedClass = "Event";
-  classNames: string[] = [
-    'BlackBoxEvent',
-    'CellDevelopmentStep',
-    'CellLineagePath',
-    'Depolymerisation',
-    'Event',
-    'FailedReaction',
-    'InteractionEvent',
-    'Pathway',
-    'Polymerisation',
-    'ReactionlikeEvent',
-    'TopLevelPathway'
-  ];
-
-  // TODO:  Retrieve all attributes of selectedClass dynamically from the Schema
-  selectedAttribute = "displayName";
-  classToAttributes: Map<string, string[]> = new Map(
-    [
-      ['BlackBoxEvent', ["displayName", "dbId"]],
-      ['CellDevelopmentStep',["displayName", "dbId"]],
-      ['CellLineagePath',["displayName", "dbId"]],
-      ['Depolymerisation',["displayName", "dbId"]],
-      ['Event',["displayName", "dbId"]],
-      ['FailedReaction',["displayName", "dbId"]],
-      ['InteractionEvent',["displayName", "dbId"]],
-      ['Pathway',["displayName", "dbId"]],
-      ['Polymerisation',["displayName", "dbId"]],
-      ['ReactionlikeEvent',["displayName", "dbId"]],
-      ['TopLevelPathway', ["displayName", "dbId"]]
-    ]);
-
-  selectedOperand = "Equals";
+  classNames: string[] = [];
+  classToAttributes: Map<string, string[]> = new Map();
   operands: string[] = [
     'Equals',
     'Contains',
@@ -79,6 +50,31 @@ export class FilterEventsComponent {
     'IS NOT NULL',
     'IS NULL'
   ];
+
+  constructor(private service: DataService) {
+    // Populate all Event (and children) classes into this.classNames
+    service.fetchSchemaClassTree().subscribe(_ => {
+      let eventSchemaClass = service.getSchemaClass("Event");
+      let classNames: Set<string> = new Set(['Event']);
+      this.getChildren(eventSchemaClass!, classNames);
+      this.classNames = Array.from(classNames).sort((a, b) => a.localeCompare(b));
+
+      // Populate className's attribute names into classToAttributes
+      this.classNames.forEach(className => {
+        this.classToAttributes.set(className, []);
+        service.fetchSchemaClass(className).subscribe(populatedSchemaClass => {
+          if (populatedSchemaClass.attributes) {
+            populatedSchemaClass.attributes.forEach(attr => {
+              this.classToAttributes.get(className)!.push(attr.name);
+            });
+            this.classToAttributes.get(className)!.sort((a, b) => a.localeCompare(b));
+          }
+        })
+      })
+    })
+  }
+
+  @Output() updateEventTree = new EventEmitter<Array<string | undefined>>();
 
   onSelectionChange(): void {
     console.debug(
@@ -93,7 +89,7 @@ export class FilterEventsComponent {
         this.selectedAttribute,
         this.selectedOperand,
         this.searchKey]);
-}
+  }
 
 
   getAttributes(className: string): string[] | undefined {
@@ -107,6 +103,20 @@ export class FilterEventsComponent {
     // no empty string sent to the server
     if (this.searchKey !== undefined && this.searchKey.length === 0) {
       this.searchKey = undefined;
+    }
+  }
+
+  /**
+   * Populate into clsNames all the recursive child classes of schemaClass
+   * @param schemaClass
+   * @param clsNames
+   */
+  getChildren(schemaClass: SchemaClass, clsNames: Set<string>): void {
+    if (schemaClass.children) {
+      schemaClass.children.forEach(child => {
+        clsNames.add(child.name);
+        this.getChildren(child, clsNames);
+      });
     }
   }
 }
