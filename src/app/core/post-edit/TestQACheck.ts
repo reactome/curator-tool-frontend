0a444738492c6007395c5358ce79790b74dd644f
Robert Petryszak <info@datasome.co.uk>
2024-04-24 23:03:23 +0100

    Prototyping a framework for running (post-edit) and reporting on QA checks (WIP)

---
+++ src/app/core/post-edit/TestQACheck.ts
@@ -0,0 +1,47 @@
+import { concatMap } from "rxjs";
+import { Instance } from "../models/reactome-instance.model";
+import { DataService } from "../services/data.service";
+import { InstanceNameGenerator } from "./InstanceNameGenerator";
+import { PostEditListener, PostEditOperation } from "./PostEditOperation";
+import { SchemaClass } from "../models/reactome-schema.model";
+import { NewInstanceActions } from "src/app/schema-view/instance/state/instance.actions";
+
+export class TestQACheck implements PostEditOperation {
+
+    constructor(private dataService: DataService) {
+    }
+
+    postEdit(instance: Instance,
+             editedAttributeName: string | undefined,
+             postEditListener: PostEditListener | undefined): boolean {
+
+       let editedAttributeNames: string[] = [];
+       let editedAttributeValues: string[] = [];
+       if (instance.modifiedAttributes !== undefined) {
+         instance.modifiedAttributes.forEach((value: string, attr: string) => {
+           editedAttributeNames.push(attr);
+           let new_val = instance.attributes.get(attr);
+           editedAttributeValues.push(new_val != null ? new_val : "null");
+         });
+       }
+
+       this.dataService.testQACheckReport(
+              instance.dbId,
+              editedAttributeNames.toString(),
+              editedAttributeValues.toString())
+          .subscribe(data => {
+          if (instance.qaIssues === undefined) {
+            instance.qaIssues = new Map();
+          }
+          instance.qaIssues.set("TestQACheck", data);
+          if (instance.qaIssues.get("TestQACheck")!.length === 0) {
+              instance.qaIssues.delete("TestQACheck");
+          }
+          console.log(instance.qaIssues);
+          if (postEditListener)
+              postEditListener.donePostEdit(instance, editedAttributeName);
+
+       });
+        return true;
+    }
+}
