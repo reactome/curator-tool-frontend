a4b5900c6a4666f64c2c0216e14cfa28608a2ce5
guanmingwu <guanmingwu@gmail.com>
2024-09-30 21:09:17 -0700

    updated bookmarks when an instance is deleted/committed

--- src/app/schema-view/instance-bookmark/components/bookmark-list/bookmark-list.component.ts
+++ src/app/schema-view/instance-bookmark/components/bookmark-list/bookmark-list.component.ts
@@ -6,6 +6,8 @@ import {bookmarkedInstances} from "../../state/bookmark.selectors";
 import {Store} from "@ngrx/store";
 import {BookmarkActions} from "../../state/bookmark.actions";
 import {Router, ActivatedRoute} from "@angular/router";
+import { InstanceUtilities } from 'src/app/core/services/instance.service';
+import { DataService } from 'src/app/core/services/data.service';
 
 @Component({
   selector: 'app-bookmark-list',
@@ -20,7 +22,28 @@ export class BookmarkListComponent implements OnInit {
   constructor(public dragDropService: DragDropService,
               public store: Store,
               private router: Router,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+            private instUtils: InstanceUtilities,
+          private dataService: DataService) {
+      // An instance that is marked as deleted should not be used
+      // This is different from the instance view: an instance is deleted by
+      // not commited can still be used in the attribute list!!!
+      // TODO: This behavior is quite confusing. Need to think more!
+      this.instUtils.markDeletionDbId$.subscribe(dbId => {
+        const found = this.bookmarks.filter(inst => inst.dbId === dbId);
+        if (found.length > 0)
+          this.store.dispatch(BookmarkActions.remove_bookmark(found[0])); // There should be only one
+    });
+    this.instUtils.committedNewInstDbId$.subscribe(([oldDbId, newDbId]) => {
+      // This will change the dbId and display name
+      const removed = this.bookmarks.filter(inst => inst.dbId === oldDbId);
+      if (removed.length > 0)
+        this.store.dispatch(BookmarkActions.remove_bookmark(removed[0]));
+      // Add the new instance
+      this.dataService.fetchInstance(newDbId).subscribe(inst => {
+        this.store.dispatch(BookmarkActions.add_bookmark(this.instUtils.makeShell(inst)));
+      });
+    });
   }
 
   ngOnInit() {
