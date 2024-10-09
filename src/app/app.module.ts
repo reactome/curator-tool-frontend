f890ec330b1a3851d2d450615f694917ff1c17f4
guanmingwu <guanmingwu@gmail.com>
2024-10-09 09:06:46 -0700

    fixed error during github merging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { MAT_TOOLTIP_DEFAULT_OPTIONS, MatTooltipDefaultOptions } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouteReuseStrategy, RouterModule } from "@angular/router";
+import { RouterModule } from "@angular/router";
 import { JwtModule } from "@auth0/angular-jwt";
 import { EffectsModule } from '@ngrx/effects';
 import { routerReducer } from '@ngrx/router-store';
@@ -12,7 +12,7 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { DIAGRAM_CONFIG_TOKEN } from 'ngx-reactome-diagram';
 import { environment } from 'src/environments/environment.dev';
-import { AppRoutingModule, routes } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from "./auth/auth.module";
 import { HeaderInterceptor } from "./core/interceptors/header.interceptor";
@@ -26,8 +26,6 @@ import { MainSchemaViewModule } from "./schema-view/main-schema-view/main-schema
 import { SchemaClassTableModule } from './schema-view/schema-class/components/table/schema-class-table.module';
 import { SharedModule } from "./shared/shared.module";
 import { StatusModule } from './status/status.module';
-import { CustomReuseStrategy } from './custom-reuse-strategy';
-// import { CustomSerializer } from "./store/custom-serializer";
 
 export function tokenGetter() {
   return localStorage.getItem("token");
