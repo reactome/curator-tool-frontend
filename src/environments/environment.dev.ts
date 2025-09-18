1788bfb1b2b8c533a9b38e317c1e8285caf31faa
guanmingwu <guanmingwu@gmail.com>
2025-09-18 16:55:50 -0700

    minor update

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
     ApiRoot: "http://localhost:9090/api/curation",
     authURL: "http://localhost:9090/api/authenticate",
-    llmURL: "http://127.0.0.1:5000"
+    llmURL: "http://127.0.0.1:5000",
+    llmOn: false
   };
